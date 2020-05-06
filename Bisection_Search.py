e00721124774ddfb280798cd0f2496c0b01d2052
Pranav 'GOTHAM' Gautam <pranavgautam@live.com>
2020-05-06 18:59:59 +0530

    same ole

--- Bisection_Search.py
+++ Bisection_Search.py
@@ -14,21 +14,24 @@ numGuesses = 0
 
 ans = (high + low)/2.0
 
-while abs(ans**2 - x) >= epsilon:
-    numGuesses += 1
-    if ans**2 < x:
-        low = ans
-    else:
-        high = ans
-    ans = (high + low)/2.0
- 
-print 'The program made ', numGuesses, ' guesses in total'
-
-if (math.ceil(ans) ** 2 == x):
-    print math.ceil(ans), ' is square root of ', x
-elif (math.floor(ans) ** 2 == x):
-    print math.floor(ans), ' is square root of ', x
+if x < 0:
+    print 'Failed on square root of ', x
 else:
-    print ans, ' is close to square root of ', x
-    print 'On squaring ', ans, ' we get ', ans**2
+    while abs(ans**2 - x) >= epsilon:
+        numGuesses += 1
+        if ans**2 < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2.0
+ 
+    print 'The program made ', numGuesses, ' guesses in total'
+
+    if (math.ceil(ans) ** 2 == x):
+        print math.ceil(ans), ' and ', -(math.ceil(ans)), ' are square roots of ', x
+    elif (math.floor(ans) ** 2 == x):
+        print math.floor(ans), ' and ', -(math.floor(ans)), ' are square roots of ', x
+    else:
+        print ans, ' and ', -ans, ' are close to square roots of ', x
+        print 'On squaring ', ans, '(or ', -ans, ') we get ', ans**2
         
--- Exhaustive_Enumeration.py
+++ Exhaustive_Enumeration.py
@@ -1,5 +1,7 @@
 #Approximate square root using Exhaustive Enumeration Search
 
+import math
+
 x = float(raw_input('Enter a float: '))
 
 epsilon = 0.01
@@ -10,13 +12,23 @@ numGuesses = 0
 
 ans = 0.0
 
-while abs(ans**2 - x) >= epsilon and ans**2 <= x:
-    ans += step
-    numGuesses += 1
- 
-print 'numGuesses = ', numGuesses
-
-if abs(ans**2 - x) >= epsilon:
+if x < 0:
     print 'Failed on square root of ', x
 else:
-    print ans, ' is close to square root of ', x
+    while abs(ans**2 - x) >= epsilon and ans**2 <= x:
+        ans += step
+        numGuesses += 1
+ 
+    print 'numGuesses = ', numGuesses
+
+    if abs(ans**2 - x) >= epsilon:
+        print 'Failed on square root of ', x
+    else:
+        if (math.ceil(ans) ** 2 == x):
+            print math.ceil(ans), ' and ', -(math.ceil(ans)), ' are square roots of ', x
+        elif (math.floor(ans) ** 2 == x):
+            print math.floor(ans), ' and ', -(math.floor(ans)), ' are square roots of ', x
+        else:
+            print ans, ' and ', -ans, ' are close to square roots of ', x
+            print 'On squaring ', ans, '(or ', -ans, ') we get ', ans**2
+
