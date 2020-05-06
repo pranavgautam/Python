2708c603a8f680f2dc0a887f893ae0b6f90bf839
Pranav 'GOTHAM' Gautam <pranavgautam@live.com>
2020-05-06 21:39:29 +0530

    Update Bisection_CubeRoot.py

--- Bisection_CubeRoot.py
+++ Bisection_CubeRoot.py
@@ -4,40 +4,32 @@ import math
 
 x = float(raw_input('Enter a float: '))
 
-y = abs(x)
-
 epsilon = 0.01
 
-low = min(-y, 0.0)
+low = min(x, 0.0)
 
-high = max(0.0, y)
+high = max(1.0, x)
 
 numGuesses = 0
 
 ans = (high + low)/2.0
 
-while abs(ans**3 - y) >= epsilon:
+while abs(ans**3 - x) >= epsilon:
     numGuesses += 1
-    if ans**3 < y:
+    if ans**3 < x:
         low = ans
+        print ans
     else:
         high = ans
+        print ans
     ans = (high + low)/2.0
  
 print 'The program made ', numGuesses, ' guesses in total'
 
-if (math.ceil(ans) ** 3 == y) and x < 0:
-    print -(math.ceil(ans)), ' is the cube root of ', x
-elif (math.ceil(ans) ** 3 == y) and x >= 0:
+if (math.ceil(ans) ** 3 == x):
     print math.ceil(ans), ' is the cube root of ', x
-elif (math.floor(ans) ** 3 == x) and x < 0:
-    print -(math.floor(ans)), ' is the cube root of ', x
-elif (math.floor(ans) ** 3 == x) and x >= 0:
+elif (math.floor(ans) ** 3 == x):
     print math.floor(ans), ' is the cube root of ', x
 else:
-    if x < 0:
-        print -ans, ' is close to cube root of ', x
-        print 'On cubing ', -ans, ' we get ', -ans**3
-    else:
-        print ans, ' is close to cube root of ', x
-        print 'On cubing ', ans, ' we get ', ans**3
+    print ans, ' is close to cube root of ', x
+    print 'On cubing ', ans, ' we get ', ans**3
